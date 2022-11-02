8d6cfda5e477d150eb9cf100d830636c18edb12e
unknown <Naseerj>
2022-11-02 10:18:59 +0100

    Added the images for landing page

--- src/Section1.js
+++ src/Section1.js
@@ -6,7 +6,10 @@ import Collection from "./Images/Vector1.svg";
 import Radio from "./Images/radio.svg";
 import Profile from "./Images/Vector3.png";
 import Logout from "./Images/Logout.svg";
-import Net from "./Images/Vector (10).png";
+import Net from "./Images/Vector (13).svg";
+import Singer from "./Images/Pexels Photo by Eric Esma.svg";
+import Pictures from './Images/Frame 4 (1).svg'
+import Likes from './Images/Frame 3.svg'
 import "./Section1.css";
 
 const Section1 = () => {
@@ -20,19 +23,31 @@ const Section1 = () => {
       </div>
       <div className="parentdiv">
         <div>
-        <div className="icondiv">
-          <img src={Home} alt="" />
-          <img src={Musiclibrary} alt="" />
-          <img src={Collection} alt="" />
-          <img src={Radio} alt="" />
-        </div>
-        <div className="icon2div">
-          <img src={Radio} alt="" />
-          <img src={Logout} alt="" />
-        </div>
+          <div className="icondiv">
+            <img src={Home} alt="" />
+            <img src={Musiclibrary} alt="" />
+            <img src={Collection} alt="" />
+            <img src={Radio} alt="" />
+          </div>
+          <div className="icon2div">
+            <img src={Radio} alt="" />
+            <img src={Logout} alt="" />
+          </div>
         </div>
         <div className="picturediv">
+          <h4 className="text1">Curated Playlist</h4>
+          <div className="RBdiv">
+          <h2>R & B HITS</h2>
+          <p className="p1">
+            R & B Hits All mine, Lie again, Petty call me everyday, Out of time,
+            No love, Bad habit, and so much more
+          </p>
+          </div>
+          <img src={Pictures} alt="" />
+          <img src={Likes} alt="" />
           <img className="net" src={Net} alt="" />
+          <img className="singer" src={Singer} alt="" />
+          <div></div>
         </div>
       </div>
     </div>
